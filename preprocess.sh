f6c349603619abdb161ec5dedb8a03092061c6fa
potentpwnables <potent_pwnables@protonmail.com>
2019-05-01 20:36:32 -0400

    removing normalization code since it didn't work

--- preprocess.sh
+++ preprocess.sh
@@ -9,8 +9,8 @@ for f in "${files[@]}"
 do
 	if [ ! -f $f ]
 	then
-		wget "https://csr.lanl.gov/data/cyber1/$f.gz" -q --show-progress --progress=bar:force:noscroll
-		gunzip "$f.gz"
+		wget https://csr.lanl.gov/data/cyber1/$f.gz -q --show-progress --progress=bar:force:noscroll
+		gunzip $f.gz
 	fi
 done
 
@@ -19,7 +19,7 @@ echo "[+] Creating initial versions of final files"
 declare -a files=('computers.csv' 'user_domains.csv' 'ports.csv' 'processes.csv' 'auth_type.csv' 'auth_orientation.csv' 'logon_type.csv')
 for f in "${files[@]}"
 do
-	echo 'id,name' > $f
+	echo "id,name" > $f
 done
 
 # extract information from the raw files into the reference files
@@ -40,31 +40,9 @@ declare -a files=('computers' 'user_domains' 'ports' 'processes' 'auth_type' 'au
 for f in "${files[@]}"
 do
         # Sort the file and make it unique, and then add line numbers
-        echo "Working on $f..."
+        echo "    Working on $f..."
         cat $f.txt | sort --parallel=4 -u | awk '{printf "%s,%s\n",NR,$0}' >> $f.csv
         rm $f.txt
 done
-
-# replace the data in the main tables with the lookup values in the reference tables
-declare -a files=('computers.csv' 'ports.csv' 'processes.csv' 'user_domains.csv' 'auth_type.csv' 'auth_orientation.csv' 'logon_type.csv')
-for f in "${files[@]}"
-do
-	sort -k 2 -t , -o $f $f
-done
-
-echo "[+] Normalizing auth.txt"
-sort -k 2 -t "," auth.txt | join -1 2 -2 2 -t , -o 1.1,2.1,1.3,1.4,1.5,1.6,1.7,1.8,1.9 - user_domains.csv | sort -k 3 -t , | join -1 3 -2 2 -t , -o 1.1,1.2,2.1,1.4,1.5,1.6,1.7,1.8,1.9 - user_domains.csv | sort -k 4 -t , | join -1 4 -2 2 -t , -o 1.1,1.2,1.3,2.1,1.5,1.6,1.7,1.8,1.9 - computers.csv | sort -k 5 -t , | join -1 5 -2 2 -t , -o 1.1,1.2,1.3,1.4,2.1,1.6,1.7,1.8,1.9 - computers.csv | sort -k 6 -t , | join -1 6 -2 2 -t , -o 1.1,1.2,1.3,1.4,1.5,2.1,1.7,1.8,1.9 - auth_type.csv | sort -k 7 -t , | join -1 7 -2 2 -t , -o 1.1,1.2,1.3,1.4,1.5,1.6,2.1,1.8,1.9 - logon_type.csv | sort -k 8 -t , | join -1 8 -2 2 -t , -o 1.1,1.2,1.3,1.4,1.5,1.6,1.7,2.1,1.9 - auth_orientation.csv | sort -k 1 -t , | sed -e "s/Success/1/g;s/Failure/0/g" > auth.csv
-
-echo "[+] Normalizing flows.txt"
-sort -k 3 -t , flows.txt | join -1 3 -2 2 -t , -o 1.1,1.2,2.1,1.4,1.5,1.6,1.7,1.8,1.9 - computers.csv | sort -k 4 -t , | join -1 4 -2 2 -t , -o 1.1,1.2,1.3,2.1,1.5,1.6,1.7,1.8,1.9 - ports.csv | sort -k 5 -t , | join -1 5 -2 2 -t , -o 1.1,1.2,1.3,1.4,2.1,1.6,1.7,1.8,1.9 - computers.csv | sort -k 6 -t , | join -1 6 -2 2 -t , -o 1.1,1.2,1.3,1.4,1.5,2.1,1.7,1.8,1.9 - ports.csv| sort -k 1 -t , > flows.csv
-
-echo "[+] Normalizing proc.txt"
-sort -k 2 -t , proc.txt | join -1 2 -2 2 -t , -o 1.1,2.1,1.3,1.4,1.5 - user_domains.csv | sort -k 3 -t , | join -1 3 -2 2 -t , -o 1.1,1.2,2.1,1.4,1.5 - computers.csv | sort -k 4 -t , | join -1 4 -2 2 -t , -o 1.1,1.2,1.3,2.1,1.5 - processes.csv| sort -k 1 -t , | sed -e "s/Start/1/g;s/End/0/g" > proc.csv
-
-echo "[+] Normalizing dns.txt"
-sort -k 2 -t , dns.txt | join -1 2 -2 2 -t , -o 1.1,2.1,1.3 - computers.csv | sort -k 3 -t , | join -1 3 -2 2 -t , -o 1.1,1.2,2.1 - computers.csv | sort -k 1 -t , > dns.csv
-
-echo "[+] Normalizing redteam.txt"
-sort -k 2 -t , redteam.txt | join -1 2 -2 2 - user_domains.csv -t , -o 1.1,2.1,1.3,1.4 | sort -k 3 -t , | join -1 3 -2 2 - computers.csv -t , -o 1.1,1.2,2.1,1.4 | sort -k 4 -t , | join -1 4 -2 2 - computers.csv -t , -o 1.1,1.2,1.3,2.1 | sort -k 1 -t , > redteam.csv
 
 cd -
